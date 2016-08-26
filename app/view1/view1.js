c06e53029cd65e0d978249467a7acecce8b2dd91
ram <ramireddy.palamakula@nextsphere.com>
2016-08-26 15:01:10 +0530

    Initial Commit

---
+++ app/view1/view1.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute','angular-gcm'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope','$http',function($scope,$http) {
+  $scope.gcmSend = function(sub) {
+              // send token to server and save it
+            //  $http.post('your_server_url', {"token" : sub.subscriptionId})
+
+          }
+
+          $scope.sends=function(notif){
+            var data ={
+              "message": notif.title,
+              to:'/topics/notify'
+
+            }
+
+                       var config = {
+                           headers : {
+                                Authorization:'key=AIzaSyDUuGIbu_7ODJnLIKiGkLACae8lwKCFlFk',
+                               'Content-Type': 'application/json'
+                           }
+                       }
+
+             $http.post('https://gcm-http.googleapis.com/gcm/send', data,config)
+          }
+}]);
