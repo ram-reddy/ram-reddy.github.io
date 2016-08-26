9d790968d65c26c5a5f75e5842d235fbc21ad2ca
ram <ramireddy.palamakula@nextsphere.com>
2016-08-26 16:36:26 +0530

    srsg

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,6 +11,9 @@ angular.module('myApp.view1', ['ngRoute','angular-gcm'])
 
 .controller('View1Ctrl', ['$scope','$http',function($scope,$http) {
   $scope.gcmSend = function(sub) {
+
+$scope.sub=sub;
+
               // send token to server and save it
             //  $http.post('your_server_url', {"token" : sub.subscriptionId})
 
